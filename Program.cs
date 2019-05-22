4832b32c3d1c77c39e9a3e317855fdbb2dd30501
HP <HP@DESKTOP-6SUN5DR>
2019-05-22 14:09:26 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the amount of celsius: ");
+            int celsius = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Kelvin = {0}", celsius + 273);
+            Console.WriteLine("Fahrenheit = {0}", celsius * 18 / 10 + 32);
+        }
+    }
+}
